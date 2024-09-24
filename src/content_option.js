3e734e8a893fc43c91c499e0464989a33c963f83
PC-AARON\aaron <acarneiro@medityapp.com>
2024-09-24 18:25:17 +0200

    cambios

--- src/content_option.js
+++ src/content_option.js
@@ -1,74 +1,66 @@
-const logotext = "JOHN";
+const logotext = "Aaron";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Aaron Carneiro",
+    description: "I’m Aaron Carneiro Front developer",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Aaron Carneiro",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
         third: "I develop mobile apps",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "I am a Front-End Developer and UX/UI Designer, crafting visually appealing and user-friendly web applications. I combine design concepts with technical skills to create responsive, intuitive interfaces that enhance user experiences.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "About myself",
+    aboutme: "I am a Front-End Developer and UX/UI Designer, crafting visually appealing and user-friendly web applications. I combine design concepts with technical skills to create responsive, intuitive interfaces that enhance user experiences.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Java developer",
+        where: "NTT Data",
+        date: "2021-2022",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+        jobtitle: "App Developer",
+        where: "Medity App",
+        date: "2023",
+    }
 ];
 
 const skills = [{
-        name: "Python",
+        name: "JavaScript",
         value: 90,
     },
     {
-        name: "Djano",
+        name: "React",
         value: 85,
     },
     {
-        name: "Javascript",
+        name: "Java",
         value: 80,
     },
     {
-        name: "React",
+        name: "Springboot",
         value: 60,
     },
-    {
-        name: "Jquery",
-        value: 85,
-    },
+   
 ];
 
 const services = [{
         title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "Responsive and attractive",
     },
     {
         title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "Build cross mobile apps with React Native",
     },
     {
         title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "Build fast, usefull and responsives websites",
     },
 ];
 
@@ -136,9 +128,9 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "aaroncarneirotorres@gmail.com",
+    YOUR_FONE: "(+34) 685 303 431",
+    description: "I am a Front-End Developer and UX/UI Designer, crafting visually appealing and user-friendly web applications. I combine design concepts with technical skills to create responsive, intuitive interfaces that enhance user experiences.",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
@@ -147,10 +139,7 @@ const contactConfig = {
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    linkedin: "www.linkedin.com/in/aaron-carneiro-014b20229",
 };
 export {
     meta,
