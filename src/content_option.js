fcb4b7f66d3ed16c2240aac3dbdfe33396330029
PC-AARON\aaron <acarneiro@medityapp.com>
2025-03-17 19:16:30 +0100

    cambios pruebas

--- src/content_option.js
+++ src/content_option.js
@@ -11,13 +11,13 @@ const introdata = {
         second: "I code cool websites",
         third: "I develop mobile apps",
     },
-    description: "I am a Front-End Developer and UX/UI Designer, crafting visually appealing and user-friendly web applications. I combine design concepts with technical skills to create responsive, intuitive interfaces that enhance user experiences.",
+    description: "I’m a React Native developer specializing in UX/UI design, building visually stunning and responsive mobile apps. I create intuitive, user-friendly experiences that adapt seamlessly to any device, blending modern design with efficient code for smooth performance and great usability.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "About myself",
-    aboutme: "I am a Front-End Developer and UX/UI Designer, crafting visually appealing and user-friendly web applications. I combine design concepts with technical skills to create responsive, intuitive interfaces that enhance user experiences.",
+    aboutme: "I am a React Native developer with a strong focus on UX/UI design, crafting visually appealing and highly responsive mobile applications. My expertise lies in creating seamless, user-friendly experiences that prioritize both aesthetics and functionality. I ensure that every app I develop is intuitive, engaging, and fully optimized for different screen sizes and devices. By combining modern design principles with clean, efficient code, I deliver applications that not only look great but also perform flawlessly. Passionate about innovation and usability, I strive to enhance digital experiences through well-designed and responsive mobile solutions.",
 };
 const worktimeline = [{
         jobtitle: "Java developer",
@@ -25,13 +25,27 @@ const worktimeline = [{
         date: "2021-2022",
     },
     {
-        jobtitle: "App Developer",
+        jobtitle: "React-Native",
         where: "Medity App",
-        date: "2023",
+        date: "2023-2024",
+    },
+    {
+        jobtitle: "React-Native Developer",
+        where: "Seidor",
+        date: "2024-2025"
     }
 ];
 
-const skills = [{
+const skills = [
+    {
+        name: "ReactNative",
+        value: 90
+    },
+    {
+        name: "Expo",
+        value: 80,
+    },
+    {
         name: "JavaScript",
         value: 90,
     },
@@ -43,10 +57,7 @@ const skills = [{
         name: "Java",
         value: 80,
     },
-    {
-        name: "Springboot",
-        value: 60,
-    },
+    
    
 ];
 
